a7729a56bb16ddbe39cfc602cf2840201aa1957b
Guilherme <guilherme.etg@outlook.com>
2022-01-26 16:19:23 -0300

    guardando

---
+++ front_end/form/README.md
@@ -0,0 +1,3 @@
+Aprendendo a fazer um form com react, não foi em quem escreveu o código, segui este tutorial - https://www.taniarascia.com/getting-started-with-react/.
+
+Mesmo não tendo escrito o código, fiquei feliz de aprender como faz o form e consegui entender mais sobre state.
---
+++ front_end/form/src/components/Form.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+
+    handleChange = (event) => {
+        const { name, value } = event.target
+
+        this.setState({
+            [name]: value,
+        })
+    }
+
+    
+    initialState = {
+        name: '',
+        job: '',
+    }
+    
+    state = this.initialState
+    
+    submitForm = () => {
+        this.props.handleSubmit(this.state)
+        this.setState(this.initialState)
+    }
+
+    render() {
+        const { name, job } = this.state;
+
+        return (
+            <form>
+                <label htmlFor="name">Nome</label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={ name }
+                    onChange={ this.handleChange } />
+
+                <label htmlFor="job">Trabalho</label>
+                <input
+                    type="text"
+                    name="job"
+                    id="job"
+                    value={ job }
+                    onChange={ this.handleChange } />
+
+                <input
+                    type="button"
+                    value="Submit"
+                    onClick={ this.submitForm }/>
+            </form>
+        )
+    }
+}
+
+export default Form
